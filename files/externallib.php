f18c8f979dcf7cccfac2a9713fb65e8b23065494
Ankit Agarwal <ankit@moodle.com>
2013-07-03 14:17:39 +0800

    MDL-40402 libraries: Replace get_system_context() with context_system::instance() in core

--- files/externallib.php
+++ files/externallib.php
@@ -80,7 +80,7 @@ class core_files_external extends external_api {
         $browser = get_file_browser();
 
         if (empty($fileinfo['contextid'])) {
-            $context  = get_system_context();
+            $context  = context_system::instance();
         } else {
             $context  = context::instance_by_id($fileinfo['contextid']);
         }
@@ -274,7 +274,7 @@ class core_files_external extends external_api {
         if (!empty($fileinfo['contextid'])) {
             $context = context::instance_by_id($fileinfo['contextid']);
         } else {
-            $context = get_system_context();
+            $context = context_system::instance();
         }
 
         if (!($fileinfo['component'] == 'user' and $fileinfo['filearea'] == 'private')) {
