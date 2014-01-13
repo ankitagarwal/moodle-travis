d30c57b4d69d4795212d184591a543b5e46ae85b
Petr Škoda <commits@skodak.org>
2014-01-13 09:08:58 +0800

    MDL-37658 add new logging API with basic implemenation

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1024,6 +1024,10 @@ class core_plugin_manager {
             'local' => array(
             ),
 
+            'logstore' => array(
+                'database', 'legacy', 'standard',
+            ),
+
             'message' => array(
                 'email', 'jabber', 'popup'
             ),
@@ -1113,7 +1117,7 @@ class core_plugin_manager {
             'tool' => array(
                 'assignmentupgrade', 'behat', 'capability', 'customlang',
                 'dbtransfer', 'generator', 'health', 'innodb', 'installaddon',
-                'langimport', 'multilangupgrade', 'phpunit', 'profiling',
+                'langimport', 'log', 'multilangupgrade', 'phpunit', 'profiling',
                 'qeupgradehelper', 'replace', 'spamcleaner', 'timezoneimport',
                 'unittest', 'uploadcourse', 'uploaduser', 'unsuproles', 'xmldb'
             ),
